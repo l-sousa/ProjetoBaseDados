4d651971a6f4113c3bee18284c16f09918f1c464
l-sousa <48656255+l-sousa@users.noreply.github.com>
2020-05-22 19:25:15 +0100

    comecei o insert do staff

--- Trabalho Final-2/Trabalho Final/Staff.cs
+++ Trabalho Final-2/Trabalho Final/Staff.cs
@@ -8,6 +8,8 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Data.SqlClient;
+using System.Runtime.CompilerServices;
+using System.Collections;
 
 namespace Trabalho_Final
 {
@@ -15,6 +17,50 @@ namespace Trabalho_Final
     public partial class Staff : UserControl
     {
         private static Staff _instance;
+
+        private Hashtable nifs_staff = getnifs();
+
+        public static Hashtable getnifs()
+        {
+            const string connectionString = @"Data Source=tcp:mednat.ieeta.pt\\SQLSERVER,8101;Initial Catalog=p5g4; user=p5g4; password=TiagoLucas2000";
+            try
+
+            {
+                SqlConnection con = new SqlConnection(connectionString);
+                if (con.State == ConnectionState.Closed)
+                    con.Open();
+
+                string cmdst = "SELECT nif,especialidade FROM PROJETO.DENTISTA";
+
+                SqlDataAdapter adap = new SqlDataAdapter(cmdst, con);
+
+                DataTable ds = new DataTable();
+
+                adap.Fill(ds);
+
+
+
+                Hashtable nifs_dentista = new Hashtable();
+
+                foreach (DataRow dr in ds.Rows)
+                {
+                    nifs_dentista.Add(dr.Field<int>("nif"), dr.Field<string>("especialidade"));
+
+                }
+
+                if (con.State == ConnectionState.Open)
+                    con.Close();
+
+                return nifs_dentista;
+            }
+
+            catch (Exception pp)
+
+            {
+                System.Windows.Forms.MessageBox.Show("Imposssível conectar!");
+                return null;
+            }
+        }
         const string connectionString = @"Data Source=tcp:mednat.ieeta.pt\\SQLSERVER,8101;Initial Catalog=p5g4; user=p5g4; password=TiagoLucas2000";
         public static Staff Instance
         {
@@ -150,7 +196,6 @@ namespace Trabalho_Final
                 showStaff(query);
             }
         }
-
         private void Assistente_CheckedChanged(object sender, EventArgs e)
         {
             if (Assistente.Checked)
@@ -171,6 +216,8 @@ namespace Trabalho_Final
 
         private void btnAdicionar_Click(object sender, EventArgs e)
         {
+
+            // VALIDAÇÃO DAS CAIXAS DE TEXTO
             string nome_box = textBox1.Text;
 
             int box_contacto;
@@ -192,7 +239,7 @@ namespace Trabalho_Final
 
             int box_nif;
             sucess = Int32.TryParse(textBox4.Text, out box_nif);
-            if (!sucess)
+            if (!sucess || nifs_staff.ContainsKey(box_nif))
             {
                 MessageBox.Show("Nif inválido");
                 textBox4.Text = "";
@@ -201,13 +248,40 @@ namespace Trabalho_Final
 
             string box_endereco = textBox3.Text;
 
-            int box_nif;
-            sucess = Int32.TryParse(textBox7.Text, out box_nif);
+            double box_salario;
+            sucess = Double.TryParse(textBox7.Text, out box_salario);
             if (!sucess)
             {
                 MessageBox.Show("Nif inválido");
-                textBox4.Text = "";
+                textBox7.Text = "";
                 return;
+            }
+
+            // CRIAR CÓDIGO NECESSÁRIO PARA O INSERT
+            /*vamos precisar de fazer dois inserts, na tabela de STAFF e na tabela de PESSOA    */
+
+            using (SqlConnection SqlConn = new SqlConnection(connectionString))
+            {
+                if (SqlConn.State == ConnectionState.Closed)
+                    SqlConn.Open();
+                //--- Pesquisa na BD
+
+                // INSERT 
+
+                string staff_query = "INSERT INTO PROJETO.STAFF(nif, salario, nif_clinica) VALUES (" + nif_clinica + ",\'" + nome + "\')";
+                using (SqlCommand cmd = new SqlCommand(, SqlConn))
+                {
+                    int rowsAdded = cmd.ExecuteNonQuery();
+                    if (rowsAdded > 0)
+                        System.Windows.Forms.MessageBox.Show("Material adicionado!");
+                    else
+                        System.Windows.Forms.MessageBox.Show("Nao foi possível adicionar novo !");
+
+                }
+
+
+                if (SqlConn.State == ConnectionState.Open)
+                    SqlConn.Close();
             }
 
         }
