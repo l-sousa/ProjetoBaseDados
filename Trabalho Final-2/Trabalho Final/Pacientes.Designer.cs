66e549d064843b18c88157f09f09c0a165fb055b
TiagoOliveira0 <60920429+TiagoOliveira0@users.noreply.github.com>
2020-05-28 18:06:30 +0100

    Tab dos pacientes acabada.

--- Trabalho Final-2/Trabalho Final/Pacientes.Designer.cs
+++ Trabalho Final-2/Trabalho Final/Pacientes.Designer.cs
@@ -48,7 +48,9 @@
             this.textBox7 = new System.Windows.Forms.TextBox();
             this.textBox8 = new System.Windows.Forms.TextBox();
             this.label7 = new System.Windows.Forms.Label();
+            this.textBox9 = new System.Windows.Forms.TextBox();
             this.label8 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -93,6 +95,7 @@
             this.btnAdicionar.TabIndex = 34;
             this.btnAdicionar.Text = "Adicionar";
             this.btnAdicionar.UseVisualStyleBackColor = true;
+            this.btnAdicionar.Click += new System.EventHandler(this.btnAdicionar_Click);
             // 
             // label6
             // 
@@ -208,10 +211,11 @@
             this.button2.TabIndex = 41;
             this.button2.Text = "Adicionar Cheque Dentista";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // textBox7
             // 
-            this.textBox7.Location = new System.Drawing.Point(80, 398);
+            this.textBox7.Location = new System.Drawing.Point(40, 396);
             this.textBox7.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.textBox7.Name = "textBox7";
             this.textBox7.Size = new System.Drawing.Size(164, 22);
@@ -219,7 +223,7 @@
             // 
             // textBox8
             // 
-            this.textBox8.Location = new System.Drawing.Point(344, 398);
+            this.textBox8.Location = new System.Drawing.Point(233, 396);
             this.textBox8.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.textBox8.Name = "textBox8";
             this.textBox8.Size = new System.Drawing.Size(164, 22);
@@ -228,26 +232,45 @@
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(79, 377);
+            this.label7.Location = new System.Drawing.Point(37, 377);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(52, 17);
             this.label7.TabIndex = 45;
             this.label7.Text = "Código";
             // 
+            // textBox9
+            // 
+            this.textBox9.Location = new System.Drawing.Point(423, 396);
+            this.textBox9.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textBox9.Name = "textBox9";
+            this.textBox9.Size = new System.Drawing.Size(164, 22);
+            this.textBox9.TabIndex = 47;
+            // 
             // label8
             // 
             this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(341, 377);
+            this.label8.Location = new System.Drawing.Point(230, 377);
             this.label8.Name = "label8";
             this.label8.Size = new System.Drawing.Size(68, 17);
-            this.label8.TabIndex = 46;
+            this.label8.TabIndex = 48;
             this.label8.Text = "Desconto";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(420, 377);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(63, 17);
+            this.label3.TabIndex = 49;
+            this.label3.Text = "Validade";
             // 
             // Pacientes
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.label3);
             this.Controls.Add(this.label8);
+            this.Controls.Add(this.textBox9);
             this.Controls.Add(this.label7);
             this.Controls.Add(this.textBox8);
             this.Controls.Add(this.textBox7);
@@ -300,6 +323,8 @@
         private System.Windows.Forms.TextBox textBox7;
         private System.Windows.Forms.TextBox textBox8;
         private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBox9;
         private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label3;
     }
 }
--- Trabalho Final-2/Trabalho Final/Pacientes.cs
+++ Trabalho Final-2/Trabalho Final/Pacientes.cs
@@ -203,6 +203,254 @@ namespace Trabalho_Final
                 return;
             }
         }
+        private void btnAdicionar_Click(object sender, EventArgs e)
+        {
+
+            //Buscar o nif da clinica
+            int nif_clinica;
+            using (SqlConnection SqlConn = new SqlConnection(connectionString))
+            {
+
+                if (SqlConn.State == ConnectionState.Closed)
+                    SqlConn.Open();
+                //--- Pesquisa na BD
+
+                // INSERT 
+                //  @especialidade varchar(35), @numero_ordem int, @nif_clinica int, @nome varchar(100), @contacto int, @nif int, @idade int, @endereco varchar(100), @salario decimal(8, 2), @profissao varchar(100) 
+
+                SqlConnection conn = new SqlConnection(connectionString);
+
+                SqlCommand cmd = new SqlCommand();
+
+                cmd.Connection = conn;
+
+                cmd.CommandType = CommandType.Text;
+                cmd.CommandText = "SELECT PROJETO.get_nif_clinica()";
+                if (SqlConn.State == ConnectionState.Closed)
+                {
+                    SqlConn.Open();
+                    nif_clinica = Int32.Parse(cmd.ExecuteScalar().ToString());
+                }
+
+
+                if (SqlConn.State == ConnectionState.Open)
+                    SqlConn.Close();
+            }
+
+            // VALIDAÇÃO DAS CAIXAS DE TEXTO
+            string nome_box = textBox1.Text;
+            if (nome_box.Length >= 100 || nome_box.Length < 1)
+            {
+                MessageBox.Show("Nome inválido");
+                textBox1.Text = "";
+                return;
+            }
+
+            int box_contacto;
+            bool sucess = Int32.TryParse(textBox6.Text, out box_contacto);
+            if (!sucess || box_contacto.ToString().Length != 9 || !box_contacto.ToString().StartsWith("91"))
+            {
+                MessageBox.Show("Contacto inválido");
+                textBox6.Text = "";
+                return;
+            }
+
+            int box_idade;
+            sucess = Int32.TryParse(textBox5.Text, out box_idade);
+            if (!sucess)
+            {
+                MessageBox.Show("Idade inválida");
+                textBox5.Text = "";
+            }
+
+            int box_nif;
+            sucess = Int32.TryParse(textBox4.Text, out box_nif);
+            if (!sucess)
+            {
+                MessageBox.Show("Nif inválido");
+                textBox4.Text = "";
+                return;
+            }
+
+            string box_endereco = textBox3.Text;
+            if (box_endereco.Length >= 100 || box_endereco.Length < 1)
+            {
+                MessageBox.Show("Endereço inválido");
+                textBox3.Text = "";
+                return;
+            }
+
+            //Inserção de Paciente
+            using (SqlConnection SqlConn = new SqlConnection(connectionString))
+            {
+                if (SqlConn.State == ConnectionState.Closed)
+                    SqlConn.Open();
+                //--- Pesquisa na BD
+
+                // INSERT 
+                //, @idade int, @endereco varchar(100), @salario decimal(8,2), @profissao varchar(100) 
+
+                SqlConnection conn = new SqlConnection(connectionString);
+                string query = "PROJETO.insert_paciente";
+                SqlCommand cmd = new SqlCommand(query, conn);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("nome", nome_box);
+                cmd.Parameters.AddWithValue("contacto", box_contacto);
+                cmd.Parameters.AddWithValue("nif", box_nif);
+                cmd.Parameters.AddWithValue("idade", box_idade);
+                cmd.Parameters.AddWithValue("endereco", box_endereco);
+                cmd.Parameters.Add("@retval", SqlDbType.Int);
+                cmd.Parameters["@retval"].Direction = ParameterDirection.Output;
+                conn.Open();
+                cmd.ExecuteNonQuery();
+                int retval = (int)cmd.Parameters["@retval"].Value;
+                conn.Close();
+
+                /*
+                if (retval == -1)
+                {
+                    MessageBox.Show("Nif já existente!");
+                    textBox4.Text = "";
+                    return;
+                }
+                else if (retval == 0)
+                {
+                    MessageBox.Show("Número da ordem ja existente!");
+                    textBox9.Text = "";
+                    return;
+                }
+                else
+                {
+                    MessageBox.Show("Adicionado!");
+                    label8.Visible = false;
+                    label9.Visible = false;
+                    label10.Visible = false;
+                    label11.Visible = false;
+                    textBox9.Visible = false;
+                    comboBox2.Visible = false;
+                }
+                */
+
+                if (SqlConn.State == ConnectionState.Open)
+                    SqlConn.Close();
+            }
+
+            //limpar as text box todas
+            textBox1.Text = "";
+            textBox3.Text = "";
+            textBox4.Text = "";
+            textBox5.Text = "";
+            textBox6.Text = "";
+
+            //fazer load do datagrid de novo
+            showPacientes("SELECT PROJETO.PESSOA.nif,endereco,contacto,idade,nome FROM PROJETO.PACIENTE JOIN PROJETO.PESSOA ON PROJETO.PACIENTE.NIF = PROJETO.PESSOA.NIF");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // VALIDAÇÃO DAS CAIXAS DE TEXTO
+            int box_codigo;
+            bool success = Int32.TryParse(textBox7.Text,out box_codigo);
+            if (!success)
+            {
+                MessageBox.Show("Código inválido");
+                textBox7.Text = "";
+                return;
+            }
+
+            string search = textBox9.Text;
+            string box_validade;
+            if (search == "")
+            {
+                MessageBox.Show("A data tem de ser preenchida!");
+                textBox9.Text = "";
+                return;
+            }
+            else
+            {
+                string[] dataspt = search.Split('/'); ;
+                box_validade = dataspt[2].ToString() + "-" + dataspt[1].ToString() + "-" + dataspt[0].ToString();
+            }
+
+            double box_desconto;
+            success = Double.TryParse(textBox8.Text, out box_desconto);
+            if (success && box_desconto.ToString().IndexOf(".") != -1)
+            {
+                if ((box_desconto.ToString().Length > 6 || textBox8.Text == "") && (box_desconto.ToString().Substring(box_desconto.ToString().IndexOf('.'))).Length - 1 > 2)
+                {
+                    System.Windows.Forms.MessageBox.Show("O campo \"desconto\" deve ter no máximo tamanho 6!");
+                    textBox8.Text = "";
+                    return;
+                }
+            }
+            else
+            {
+                if (box_desconto.ToString().Length > 6 || textBox8.Text == "")
+                {
+                    System.Windows.Forms.MessageBox.Show("O campo \"desconto\" deve ter no máximo tamanho 5 em que 2 tem de ser decimais!");
+                    textBox8.Text = "";
+                    return;
+                }
+            }
+
+
+            //Inserção de Cheque dentista
+            using (SqlConnection SqlConn = new SqlConnection(connectionString))
+            {
+                if (SqlConn.State == ConnectionState.Closed)
+                    SqlConn.Open();
+                //--- Pesquisa na BD
+
+                // INSERT 
+
+                SqlConnection conn = new SqlConnection(connectionString);
+                string query = "PROJETO.insert_checkdentista";
+                SqlCommand cmd = new SqlCommand(query, conn);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("codigo", box_codigo);
+                cmd.Parameters.AddWithValue("desconto", box_desconto);
+                cmd.Parameters.AddWithValue("validade", box_validade);
+                cmd.Parameters.Add("@retval", SqlDbType.Int);
+                cmd.Parameters["@retval"].Direction = ParameterDirection.Output;
+                conn.Open();
+                cmd.ExecuteNonQuery();
+                int retval = (int)cmd.Parameters["@retval"].Value;
+                conn.Close();
+
+                /*
+                if (retval == -1)
+                {
+                    MessageBox.Show("Nif já existente!");
+                    textBox4.Text = "";
+                    return;
+                }
+                else if (retval == 0)
+                {
+                    MessageBox.Show("Número da ordem ja existente!");
+                    textBox9.Text = "";
+                    return;
+                }
+                else
+                {
+                    MessageBox.Show("Adicionado!");
+                    label8.Visible = false;
+                    label9.Visible = false;
+                    label10.Visible = false;
+                    label11.Visible = false;
+                    textBox9.Visible = false;
+                    comboBox2.Visible = false;
+                }
+                */
+
+                if (SqlConn.State == ConnectionState.Open)
+                    SqlConn.Close();
+            }
+
+            //limpar as text box todas
+            textBox7.Text = "";
+            textBox8.Text = "";
+            textBox9.Text = "";
+        }
 
         /*
         private void Form1_Resize(object sender, EventArgs e)
